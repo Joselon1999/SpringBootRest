da34800889910460734026c0a85e23536594289c
Joselon <joselon1999@hotmail.com>
2018-10-01 21:09:01 -0500

    Rest PreAlfa: Tutorial

---
+++ src/main/java/com/example/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,18 @@
+package com.example.rest.webservices.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+
+
+
+    private final String message;
+
+    public HelloWorldBean(String message) {
+
+        this.message = message;
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
