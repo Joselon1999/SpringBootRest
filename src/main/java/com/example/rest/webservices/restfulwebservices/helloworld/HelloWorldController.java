bcbd5460d29817273aab502d3ac020ca47465ba2
Joselon1999 <joselon1999@hotmail.com>
2018-10-05 16:43:32 -0500

    Ty

--- src/main/java/com/example/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,13 +2,22 @@ package com.example.rest.webservices.restfulwebservices.helloworld;
 
 
 import com.example.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
+import org.apache.tomcat.util.http.parser.AcceptLanguage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    public MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
@@ -23,6 +32,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello world %s",name));
+    }
+
+    @GetMapping(path = "/hello-world-international")
+    public String helloWorldinternational(@RequestHeader(name = "Accept-Language",required = false) Locale locale){
+        return messageSource.getMessage("good.morning.message",null,locale);
     }
 
 }
